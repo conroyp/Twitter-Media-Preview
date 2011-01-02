9f29700dcfc9e4f1f0c9f243bead2bd72ce6003c
Paul Conroy <paul@daft.ie>
2011-01-02 19:24:43 +0000

    Following mod recommendation to load jquery in to tmp namespace

--- chrome/content/twittermediapreview.js
+++ chrome/content/twittermediapreview.js
@@ -1,5 +1,15 @@
-var jqLoaded = false;
-
+/**
+ * Brains of the operation here. On first load on twitter.com, load jquery up in
+ * noconcflict mode (tends to kill other extensions otherwise, namely Web Developer
+ * toolbar).
+ * When on twitter.com, attach the media type definitions and subsequent loading
+ * actions on tweet expansion.
+ *
+ * Currently supported sites:
+ * - adverts.ie
+ * - daft.ie
+ * - imgur.com
+ */
 var twitterMediaPreview = function () {
     return {
         init : function () {
@@ -15,18 +25,19 @@ var twitterMediaPreview = function () {
 
                 // Run automatically on twitter
                 if (onTwitter) {
-                    if(!jqLoaded)
+
+                    // Just want to load jQuery the one time
+                    if(!twitterMediaPreview.jQuery)
                     {
                         var jsLoader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"].getService(Components.interfaces.mozIJSSubScriptLoader);
 
-                        // @TODO: Only load once
-                        jsLoader.loadSubScript("chrome://twittermediapreview/content/jquery-1.4.4.min.js");
+                        // Load jquery in to the twitterMediaPreview namespace
+                        jsLoader.loadSubScript("chrome://twittermediapreview/content/jquery-1.4.4.min.js", twitterMediaPreview);
 
                         // Set up noconflict jQuery so we don't kill other extensions
-                        jQuery.noConflict();
-
-                        // Set semaphore so we don't reload it needlessly
-                        jqLoaded = true;
+                        // tmp.jQuery is coming up as undefined after loader above, despite NS being set.
+                        // Get around this by explicitly assigning it here.
+                        twitterMediaPreview.jQuery = jQuery.noConflict(true);
                     }
 
                     twitterMediaPreview.run();
@@ -53,13 +64,14 @@ var twitterMediaPreview = function () {
                     ,render:function(B) {
                         var img = 'http://i.imgur.com/{src}';
                         var A='<div class=\'twitpic\'><a class=\'inline-media-image\' href=\''+img+'\' target=\'_blank\'> <img src=\''+img+'\' /></a></div>';
-                        jQuery(B, doc).append(window.top.getBrowser().selectedBrowser.contentWindow.wrappedJSObject.twttr.supplant(A,this.data))
+                        twitterMediaPreview.jQuery(B, doc).append(window.top.getBrowser().selectedBrowser.contentWindow.wrappedJSObject.twttr.supplant(A,this.data))
                     }
                 }
             );
 
 
             // Daft.ie SMI
+            // @TODO: Be smarter here and use this.jQuery rather than twittermediapreview.jquery
             window.top.getBrowser().selectedBrowser.contentWindow.wrappedJSObject.twttr.mediaType('twttr.media.types.Daft',
                 {icon:'photo'
                 ,domain:'http://www.daft.ie/'
@@ -77,25 +89,25 @@ var twitterMediaPreview = function () {
                     var dt = this.data;
 
                     var srcUrl = 'http://www.daft.ie/'+this.slug;
-                    jQuery.ajax({
+                    twitterMediaPreview.jQuery.ajax({
                         url: srcUrl,
                         success: function(d) {
                             var dommer = HTMLParser(d);
-                            address = jQuery(dommer).find('#smi_title h1').text();
-                            price = jQuery(dommer).find('#price').text();
+                            address = twitterMediaPreview.jQuery(dommer).find('#smi_title h1').text();
+                            price = twitterMediaPreview.jQuery(dommer).find('#price').text();
 
                             // Finance strap-line corrupts, strip it out..
-                            var finance = jQuery(dommer).find('#finance').text();
+                            var finance = twitterMediaPreview.jQuery(dommer).find('#finance').text();
                             price = price.replace(finance, '');
 
-                            details = jQuery(dommer).find('#smi_prop_type').text();
-                            photo = jQuery(dommer).find('.p1 img').attr('src');
+                            details = twitterMediaPreview.jQuery(dommer).find('#smi_prop_type').text();
+                            photo = twitterMediaPreview.jQuery(dommer).find('.p1 img').attr('src');
 
                            // If we haven't gotten an address, chances are this is a dead page/non-previewable
                             if(address.length > 0)
                             {
                                 var A='<div class=\'twitpic\'><strong><a class="inline-media-image" href="'+srcUrl+'" target="_blank">'+address+'</a><strong><br />'+details+', '+price+'<br /><a class="inline-media-image" href="'+srcUrl+'" target="_blank"><img src=\''+photo+'\' /></a></div>';
-                                jQuery(B, doc).append(window.top.getBrowser().selectedBrowser.contentWindow.wrappedJSObject.twttr.supplant(A,dt));
+                                twitterMediaPreview.jQuery(B, doc).append(window.top.getBrowser().selectedBrowser.contentWindow.wrappedJSObject.twttr.supplant(A,dt));
                             }
                         }
                     });
@@ -103,61 +115,63 @@ var twitterMediaPreview = function () {
             });
 
             // Adverts.ie SMI
+            // @TODO: Be smarter here and use this.jQuery rather than twittermediapreview.jquery
             window.top.getBrowser().selectedBrowser.contentWindow.wrappedJSObject.twttr.mediaType('twttr.media.types.Adverts',
-                    {icon:'photo'
-                    ,domain:'http://www.adverts.ie/'
-                    ,matchers:{media:/adverts.ie\/([a-zA-Z0-9\.\/\?\-\_\=]+)/g}
-                    ,process:function(A){
-                        this.data.src=this.slug;
-                        this.data.name=this.constructor._name;
-                        A()
-                    }
-                    ,render:function(B) {
-                        var title = '';
-                        var price = '';
-                        var photo = '';
-
-                        var dt = this.data;
-
-                        var srcUrl = 'http://www.adverts.ie/'+this.slug;
-						jQuery.ajax({
-                            url: srcUrl,
-                            success: function(d) {
-                                var dommer = HTMLParser(d);
-                                title = jQuery(dommer).find('.title h1').text();
-
-                                details = jQuery(dommer).find('.price dd').text();
-
-                                // Get rid of extra html string
-                                details = details.replace('Make an offer\/Ask a question', '');
-                                photo = jQuery(dommer).find('.p1 img').attr('src');
-                                // If we haven't gotten a title, chances are this is a dead page/non-previewable
-                                if(title.length > 0)
-                                {
-                                    var A='<div class=\'twitpic\'><strong><a class="inline-media-image" href="'+srcUrl+'" target="_blank">'+title+'</a><strong><br />'+details+'<br /><a class="inline-media-image" href="'+srcUrl+'" target="_blank"><img src="'+photo+'" /></a></div>';
-                                    jQuery(B, doc).append(window.top.getBrowser().selectedBrowser.contentWindow.wrappedJSObject.twttr.supplant(A,dt));
-                                }
-                            }
-                        });
-                    }
+                {icon:'photo'
+                ,domain:'http://www.adverts.ie/'
+                ,matchers:{media:/adverts.ie\/([a-zA-Z0-9\.\/\?\-\_\=]+)/g}
+                ,process:function(A){
+                    this.data.src=this.slug;
+                    this.data.name=this.constructor._name;
+                    A()
                 }
-            );}
-        };
-    }();
+                ,render:function(B) {
+                    var title = '';
+                    var price = '';
+                    var photo = '';
+
+                    var dt = this.data;
+
+                    var srcUrl = 'http://www.adverts.ie/'+this.slug;
+                    twitterMediaPreview.jQuery.ajax({
+                        url: srcUrl,
+                        success: function(d) {
+                            var dommer = HTMLParser(d);
+                            title = twitterMediaPreview.jQuery(dommer).find('.title h1').text();
+
+                            details = twitterMediaPreview.jQuery(dommer).find('.price dd').text();
+
+                            // Get rid of extra html string
+                            details = details.replace('Make an offer\/Ask a question', '');
+                            photo = twitterMediaPreview.jQuery(dommer).find('.p1 img').attr('src');
+                            // If we haven't gotten a title, chances are this is a dead page/non-previewable
+                            if(title.length > 0)
+                            {
+                                var A='<div class=\'twitpic\'><strong><a class="inline-media-image" href="'+srcUrl+'" target="_blank">'+title+'</a><strong><br />'+details+'<br /><a class="inline-media-image" href="'+srcUrl+'" target="_blank"><img src="'+photo+'" /></a></div>';
+                                twitterMediaPreview.jQuery(B, doc).append(window.top.getBrowser().selectedBrowser.contentWindow.wrappedJSObject.twttr.supplant(A,dt));
+                            }
+                        }
+                    });
+                }
+            }
+        );}
+    };
+}();
 
 /**
  * This handy number lets us get a usable DOM from a full html page (aHTMLString)
  */
-function HTMLParser(aHTMLString){
-  var html = document.implementation.createDocument("http://www.w3.org/1999/xhtml", "html", null),
+function HTMLParser(aHTMLString)
+{
+    var html = document.implementation.createDocument("http://www.w3.org/1999/xhtml", "html", null),
     body = document.createElementNS("http://www.w3.org/1999/xhtml", "body");
-  html.documentElement.appendChild(body);
+    html.documentElement.appendChild(body);
 
-  body.appendChild(Components.classes["@mozilla.org/feed-unescapehtml;1"]
-    .getService(Components.interfaces.nsIScriptableUnescapeHTML)
-    .parseFragment(aHTMLString, false, null, body));
+    body.appendChild(Components.classes["@mozilla.org/feed-unescapehtml;1"]
+        .getService(Components.interfaces.nsIScriptableUnescapeHTML)
+        .parseFragment(aHTMLString, false, null, body));
 
-  return body;
+    return body;
 }
 
 window.addEventListener("load", twitterMediaPreview.init, false);
